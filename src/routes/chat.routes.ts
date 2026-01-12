8ebe90484c4e286d3706477579b57c9884a7243f
Arowolo Abdulazeez <abdulazeezarowolo@gmail.com>
2026-01-12 21:40:58 +0100

    setting up chat router

---
+++ src/routes/chat.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { ChatHandlerController } from "../controllers/chatHandler.controller";
+
+const router = Router();
+
+router.post("/chat", ChatHandlerController);
+
+export default router;
